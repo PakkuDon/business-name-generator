a63c0742bd1d6eb7404dbc08bbf5e9652ba81035
PakkuDon <pakku.pd@gmail.com>
2025-04-17 10:11:10 +1000

    Generate business name when button is clicked

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const submitButton = document.querySelector("button[type='submit']")
+  const businessnames = []
+
+  // Code reused from Wall of Names sharks.js
+  // Generate business name on submit
+  document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    // Spawn new element on submit button with random velocity values
+    const { top, left } = submitButton.getBoundingClientRect()
+    const name = {
+      x: left,
+      y: top,
+      dX: (Math.floor(Math.random() * 50) + 10) * (Math.random() > 0.5 ? 1 : -1),
+      dY: (Math.floor(Math.random() * 50) + 10) * (Math.random() > 0.5 ? 1 : -1),
+    }
+
+    // Create element for business name
+    const elem = document.createElement("div")
+    elem.className = "business-name text-2xl"
+    elem.style.top = `${name.y}px`
+    elem.style.left = `${name.x}px`
+    elem.style.animationDuration = `${Math.floor((Math.random() * 5) + 1)}s`
+
+    // Add div for element to page
+    name.elem = elem
+    businessnames.push(name)
+    document.body.appendChild(elem)
+  })
+})
